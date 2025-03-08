8aec9bf8837b1a1e5361e19bda21379f2a04124e
Shanmukha Sahiti Challa <shanmukhasahiti@ucsb.edu>
2025-03-07 17:46:48 -0800

    reduced n/w delay

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,6 +1,6 @@
 pub const PROXY_INSTANCE_ID: u64 = 0;
 pub const CLIENT_INSTANCE_ID: u64 = 10;
 pub const PROXY_PORT: u16 = 3000;
-pub const PACKET_PROCESS_DELAY: u64 = 500;
+pub const PACKET_PROCESS_DELAY: u64 = 50;
 pub const HEARTBEAT_TIMEOUT: u64 = 5000;
 pub const ABORT_TIMEOUT: u64 = 15000;
